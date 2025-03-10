4d67c8a9eb998f9ccb71cf6e0efaa3a7998bc0a3
Lyall Ulric <lyallulric@gmail.com>
2025-03-09 20:33:31 -0600

    Revert "Merge branch 'SlimeVR:master' into master"
    This reverts commit 9cefd5f4219434ba05d8d9f8d9b15ce9b99a37a6, reversing
    changes made to cc0a07ee61071c4343f0b668bd9ac076a7fa9832.

--- src/sensor/sensor.c
+++ src/sensor/sensor.c
@@ -331,7 +331,6 @@ void sensor_retained_write(void)  // TODO: move to sys?
 {
 	if (!sensor_fusion_init) {
 		return;
-	}
 	memcpy(retained->magBias, sensor_calibration_get_magBias(), sizeof(retained->magBias));
 	sensor_fusion->save(retained->fusion_data);
 	retained->fusion_id = fusion_id;
@@ -451,7 +450,6 @@ int main_imu_init(void) {
 		sensor_calibrate_imu(sensor_imu, &sensor_imu_dev);
 	} else {
 		sensor_calibration_validate();
-	}
 	if (sensor_imu == &sensor_imu_bmi270) // bmi270 specific
 	{
 		LOG_INF("Applying gyroscope gain");
